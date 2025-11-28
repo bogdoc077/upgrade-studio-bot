56c44d14392972eece5957a9b2ea1c62585de198
Solution d.a. <admin@solution-da.com>
2025-11-28 23:59:50 +0200

    Fix API server port to 8001

--- api/server.py
+++ api/server.py
@@ -1478,4 +1478,4 @@ async def get_broadcast_detail(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8001)
